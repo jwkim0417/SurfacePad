0e522fdd54ed70d2ff5b5bee2be632092eef624a
jwkim0417 <jwkim0417@postech.ac.kr>
2021-04-14 12:04:41 +0900

    Create Android Studio Project

---
+++ SurfacePad/app/src/main/java/android/example/surfacepad/MainActivity.java
@@ -0,0 +1,31 @@
+package android.example.surfacepad;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.example.surfacepad.ui.main.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        tabs.setTabTextColors(Color.parseColor("#727272"), Color.parseColor("#ffffff"));
+    }
+}
