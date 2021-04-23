3445aba043e1a0b2c502e1ae81324455cb9bebe3
jwkim0417 <jwkim0417@postech.ac.kr>
2021-04-23 21:12:54 +0900

    Change location of folder for saving audio data & convert method

--- SurfacePad/app/src/main/java/android/example/surfacepad/MainActivity.java
+++ SurfacePad/app/src/main/java/android/example/surfacepad/MainActivity.java
@@ -22,7 +22,6 @@ import android.example.surfacepad.ui.main.SectionsPagerAdapter;
 public class MainActivity extends AppCompatActivity {
 
     private final int RECORD_PERMISSION_CODE = 9909;
-    private final int READES_PERMISSION_CODE = 9919;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, RECORD_PERMISSION_CODE);
-        }
-        else if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READES_PERMISSION_CODE);
         }
         else {
             SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
@@ -50,24 +46,6 @@ public class MainActivity extends AppCompatActivity {
             case RECORD_PERMISSION_CODE:
             {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READES_PERMISSION_CODE);
-                    } else {
-                        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-                        ViewPager viewPager = findViewById(R.id.view_pager);
-                        viewPager.setAdapter(sectionsPagerAdapter);
-                        TabLayout tabs = findViewById(R.id.tabs);
-                        tabs.setupWithViewPager(viewPager);
-                    }
-                }
-                else {
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, RECORD_PERMISSION_CODE);
-                }
-                break;
-            }
-            case READES_PERMISSION_CODE:
-            {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
                     ViewPager viewPager = findViewById(R.id.view_pager);
                     viewPager.setAdapter(sectionsPagerAdapter);
@@ -75,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                     tabs.setupWithViewPager(viewPager);
                 }
                 else {
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READES_PERMISSION_CODE);
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, RECORD_PERMISSION_CODE);
                 }
                 break;
             }
--- SurfacePad/app/src/main/java/android/example/surfacepad/ui/main/TrainingFragment.java
+++ SurfacePad/app/src/main/java/android/example/surfacepad/ui/main/TrainingFragment.java
@@ -1,25 +1,14 @@
 package android.example.surfacepad.ui.main;
 
-import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Context;
-import android.content.res.AssetFileDescriptor;
-import android.database.Cursor;
 import android.media.AudioFormat;
-import android.media.AudioManager;
 import android.media.AudioRecord;
 import android.media.AudioTrack;
 import android.media.MediaRecorder;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.os.Environment;
-import android.os.ParcelFileDescriptor;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,40 +18,24 @@ import android.widget.Button;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.channels.FileChannel;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Objects;
-import java.util.Random;
 
 public class TrainingFragment extends Fragment {
+    private static final String TAG = "TRAINING";
 
     private final int mAudioSource = MediaRecorder.AudioSource.MIC;
-    //private final int mAudioSource = MediaRecorder.AudioSource.MIC;
-    private final int mSampleRate = 48000;
+    private final int mSampleRate = 44100;
     private final short mChannelCount = AudioFormat.CHANNEL_IN_STEREO;
     private final short mAudioFormat = AudioFormat.ENCODING_PCM_16BIT;
     private final int mBufferSize = AudioTrack.getMinBufferSize(mSampleRate, mChannelCount, mAudioFormat);
-    private String mFilepath = "";
     private String mFilename = "";
 
-    public AudioRecord mAudioRecord = null;
-    public AudioTrack mAudioTrack = null;
-    public Thread mRecordThread = null;
-    public Thread mPlayThread = null;
-    public boolean isRecording = false;
-    public boolean isPlaying = false;
+    private AudioRecord mAudioRecord = null;
+    private Thread mRecordThread = null;
+    private boolean isRecording = false;
 
     public TrainingFragment() {
         // Required empty public constructor
@@ -70,8 +43,8 @@ public class TrainingFragment extends Fragment {
 
 //    private AudioRecord findAudioRecord() {
 //        for (int rate : new int[] {44100, 48000}) {
-//            for (short audioFormat : new short[] { AudioFormat.ENCODING_PCM_8BIT, AudioFormat.ENCODING_PCM_16BIT }) {
-//                for (short channelConfig : new short[] { AudioFormat.CHANNEL_IN_MONO, AudioFormat.CHANNEL_IN_STEREO }) {
+//            for (short audioFormat : new short[] { AudioFormat.ENCODING_PCM_16BIT, AudioFormat.ENCODING_PCM_8BIT }) {
+//                for (short channelConfig : new short[] { AudioFormat.CHANNEL_IN_STEREO, AudioFormat.CHANNEL_IN_MONO }) {
 //                    try {
 //                        Log.d("AudiopRecording", "Attempting rate " + rate + "Hz, bits: " + audioFormat + ", channel: " + channelConfig);
 //                        int bufferSize = AudioRecord.getMinBufferSize(rate, channelConfig, audioFormat);
@@ -95,99 +68,38 @@ public class TrainingFragment extends Fragment {
         super.onCreate(savedInstanceState);
         mAudioRecord = new AudioRecord(mAudioSource, mSampleRate, mChannelCount, mAudioFormat, mBufferSize);
         //mAudioRecord = findAudioRecord();
-
         mAudioRecord.startRecording();
-        mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, mSampleRate, mChannelCount, mAudioFormat, mBufferSize, AudioTrack.MODE_STREAM);
 
         mRecordThread = new Thread(() -> {
             SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-MM-dd-hh:mm:ss");
-            mFilename = simpleDate.format(new Date(System.currentTimeMillis())) + ".wav";
+            mFilename = simpleDate.format(new Date(System.currentTimeMillis())) + ".pcm";
             Log.d("FILENAME", mFilename);
-            ContentValues values = new ContentValues();
-            values.put(MediaStore.Audio.Media.RELATIVE_PATH, "Music/Training");
-            //values.put(MediaStore.Audio.Media.DISPLAY_NAME, "hi.wav");
-            values.put(MediaStore.Audio.Media.DISPLAY_NAME, mFilename);
-            values.put(MediaStore.Audio.Media.MIME_TYPE, "audio/*");
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                values.put(MediaStore.Audio.Media.IS_PENDING, 1);
-            }
 
-            ContentResolver contentResolver = Objects.requireNonNull(getContext()).getContentResolver();
-            Uri item = contentResolver.insert(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, values);
-
+            byte[] readData = new byte[mBufferSize];
+            FileOutputStream fos = null;
             try {
-                ParcelFileDescriptor pdf = contentResolver.openFileDescriptor(item, "w", null);
-                if (pdf == null) {
-                    Log.d("PDFF", "null");
-                } else {
-                    byte[] readData = new byte[mBufferSize];
-                    FileOutputStream fos = new FileOutputStream(pdf.getFileDescriptor());
-                    //mFilepath = getRealPathFromURI(getContext(), item);
-                    Log.d("FILEPATH", mFilepath);
-                    while (isRecording) {
-                        int ret = mAudioRecord.read(readData, 0, mBufferSize);
-                        Log.d("TAG", "read bytes is " + ret);
-                        try {
-                            writeWavHeader(fos, mChannelCount, mSampleRate, mAudioFormat);
-                            fos.write(readData, 0, mBufferSize);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    mAudioRecord.stop();
-                    mAudioRecord.release();
-                    mAudioRecord = null;
-
-                    fos.close();
-
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                        values.clear();
-                        values.put(MediaStore.Audio.Media.IS_PENDING, 0);
-                        contentResolver.update(item, values, null, null);
-                    }
-
-                    AssetFileDescriptor adf = contentResolver.openAssetFileDescriptor(item, "r", null);
-                    long length = adf.getLength();
-                    updateWavHeader(pdf.getFileDescriptor(), length);
-//                    File wav = new File(mFilepath);
-//                    updateWavHeader(wav);
-                    Log.d("UPDATE", "DONE");
-                }
+                fos = getContext().openFileOutput(mFilename, Context.MODE_APPEND);
+                Log.d("PATH", getContext().getFileStreamPath(mFilename).toString());
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        });
 
-        mPlayThread = new Thread(() -> {
-            byte[] writeData = new byte[mBufferSize];
-            FileInputStream fis = null;
-            try {
-                fis = new FileInputStream(mFilepath);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-
-            DataInputStream dis = new DataInputStream(fis);
-            mAudioTrack.play();
-
-            while (isPlaying) {
+            while(isRecording) {
+                int ret = mAudioRecord.read(readData, 0, mBufferSize);
+                Log.d(TAG, "read bytes is " + ret);
                 try {
-                    int ret = dis.read(writeData, 0, mBufferSize);
-                    if (ret <= 0) {
-
-                    }
+                    fos.write(readData, 0, mBufferSize);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-            mAudioTrack.stop();
-            mAudioTrack.release();
-            mAudioTrack = null;
+
+            mAudioRecord.stop();
+            mAudioRecord.release();
+            mAudioRecord = null;
 
             try {
-                dis.close();
-                fis.close();
+                fos.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -205,9 +117,7 @@ public class TrainingFragment extends Fragment {
     public void onViewCreated (@NotNull View view,
                                Bundle savedInstanceState) {
         Button bt_record = view.findViewById(R.id.bt_record);
-        Button bt_play = view.findViewById(R.id.bt_play);
         bt_record.setOnClickListener(this::onRecord);
-        bt_play.setOnClickListener(this::onPlay);
     }
 
     public void onRecord(View view) {
@@ -225,96 +135,6 @@ public class TrainingFragment extends Fragment {
                 mAudioRecord.startRecording();
             }
             mRecordThread.start();
-        }
-    }
-
-    public void onPlay(View view) {
-        Button b = (Button)view;
-        if (isPlaying) {
-            isPlaying = false;
-            b.setText("Play");
-        }
-        else {
-            isPlaying = true;
-            b.setText("Stop");
-
-            if(mAudioTrack == null) {
-                mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, mSampleRate, mChannelCount, mAudioFormat, mBufferSize, AudioTrack.MODE_STREAM);
-            }
-            mPlayThread.start();
-        }
-    }
-
-    public static void writeWavHeader(OutputStream out, short channels, int sampleRate, short bitDepth) throws IOException {
-        byte[] littleBytes = ByteBuffer
-                .allocate(14)
-                .order(ByteOrder.LITTLE_ENDIAN)
-                .putShort(channels)
-                .putInt(sampleRate)
-                .putInt(sampleRate * channels * (bitDepth / 8))
-                .putShort((short) (channels * (bitDepth / 8)))
-                .putShort(bitDepth)
-                .array();
-        out.write(new byte[]{
-                'R', 'I', 'F', 'F', // Chunk ID
-                0, 0, 0, 0, // Chunk Size
-                'W', 'A', 'V', 'E', // Format
-                'f', 'm', 't', ' ', //Chunk ID
-                16, 0, 0, 0, // Chunk Size
-                1, 0, // AudioFormat
-                littleBytes[0], littleBytes[1], // Num of Channels
-                littleBytes[2], littleBytes[3], littleBytes[4], littleBytes[5], // SampleRate
-                littleBytes[6], littleBytes[7], littleBytes[8], littleBytes[9], // Byte Rate
-                littleBytes[10], littleBytes[11], // Block Align
-                littleBytes[12], littleBytes[13], // Bits Per Sample
-                'd', 'a', 't', 'a', // Chunk ID
-                0, 0, 0, 0, //Chunk Size (나중에 업데이트 될 것)
-        });
-    }
-
-    public static void updateWavHeader(FileDescriptor fd, long length) throws IOException {
-        byte[] sizes1 = ByteBuffer
-                .allocate(4)
-                .order(ByteOrder.LITTLE_ENDIAN)
-                .putInt((int) (length - 8))
-                .array();
-        byte[] sizes2 = ByteBuffer
-                .allocate(4)
-                .order(ByteOrder.LITTLE_ENDIAN)
-                .putInt((int) (length - 44))
-                .array();
-        FileOutputStream fos = new FileOutputStream(fd);
-        try {
-            FileChannel ch = fos.getChannel();
-            ch.position(4);
-            ch.write(ByteBuffer.wrap(sizes1));
-            ch.position(40);
-            ch.write(ByteBuffer.wrap(sizes2));
-        } catch (IOException e) {
-            throw e;
-        } finally {
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (IOException e) {
-
-                }
-            }
-        }
-    }
-
-    public String getRealPathFromURI(Context context, Uri contentUri) {
-        Cursor cursor = null;
-        try {
-            String[] proj = { MediaStore.Audio.Media.DATA };
-            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
         }
     }
 }
