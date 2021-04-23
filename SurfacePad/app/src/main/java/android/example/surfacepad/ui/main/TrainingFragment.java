69c04b4f306ef3ecb991b4c6af88c2acb76c9e0e
jwkim0417 <jwkim0417@postech.ac.kr>
2021-04-23 18:37:00 +0900

    convert pcm to wav

--- SurfacePad/app/src/main/java/android/example/surfacepad/ui/main/TrainingFragment.java
+++ SurfacePad/app/src/main/java/android/example/surfacepad/ui/main/TrainingFragment.java
@@ -3,6 +3,7 @@ package android.example.surfacepad.ui.main;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.database.Cursor;
 import android.media.AudioFormat;
 import android.media.AudioManager;
@@ -29,13 +30,21 @@ import android.widget.Button;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+import java.util.Random;
 
 public class TrainingFragment extends Fragment {
 
@@ -46,6 +55,7 @@ public class TrainingFragment extends Fragment {
     private final short mAudioFormat = AudioFormat.ENCODING_PCM_16BIT;
     private final int mBufferSize = AudioTrack.getMinBufferSize(mSampleRate, mChannelCount, mAudioFormat);
     private String mFilepath = "";
+    private String mFilename = "";
 
     public AudioRecord mAudioRecord = null;
     public AudioTrack mAudioTrack = null;
@@ -90,15 +100,19 @@ public class TrainingFragment extends Fragment {
         mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, mSampleRate, mChannelCount, mAudioFormat, mBufferSize, AudioTrack.MODE_STREAM);
 
         mRecordThread = new Thread(() -> {
+            SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-MM-dd-hh:mm:ss");
+            mFilename = simpleDate.format(new Date(System.currentTimeMillis())) + ".wav";
+            Log.d("FILENAME", mFilename);
             ContentValues values = new ContentValues();
             values.put(MediaStore.Audio.Media.RELATIVE_PATH, "Music/Training");
-            values.put(MediaStore.Audio.Media.DISPLAY_NAME, "hi.wav");
+            //values.put(MediaStore.Audio.Media.DISPLAY_NAME, "hi.wav");
+            values.put(MediaStore.Audio.Media.DISPLAY_NAME, mFilename);
             values.put(MediaStore.Audio.Media.MIME_TYPE, "audio/*");
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                 values.put(MediaStore.Audio.Media.IS_PENDING, 1);
             }
 
-            ContentResolver contentResolver = getContext().getContentResolver();
+            ContentResolver contentResolver = Objects.requireNonNull(getContext()).getContentResolver();
             Uri item = contentResolver.insert(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, values);
 
             try {
@@ -108,8 +122,9 @@ public class TrainingFragment extends Fragment {
                 } else {
                     byte[] readData = new byte[mBufferSize];
                     FileOutputStream fos = new FileOutputStream(pdf.getFileDescriptor());
-                    Log.d("FILEPATH", getRealPathFromURI(getContext(), item));
-                    while(isRecording) {
+                    //mFilepath = getRealPathFromURI(getContext(), item);
+                    Log.d("FILEPATH", mFilepath);
+                    while (isRecording) {
                         int ret = mAudioRecord.read(readData, 0, mBufferSize);
                         Log.d("TAG", "read bytes is " + ret);
                         try {
@@ -131,46 +146,50 @@ public class TrainingFragment extends Fragment {
                         values.put(MediaStore.Audio.Media.IS_PENDING, 0);
                         contentResolver.update(item, values, null, null);
                     }
+
+                    AssetFileDescriptor adf = contentResolver.openAssetFileDescriptor(item, "r", null);
+                    long length = adf.getLength();
+                    updateWavHeader(pdf.getFileDescriptor(), length);
+//                    File wav = new File(mFilepath);
+//                    updateWavHeader(wav);
+                    Log.d("UPDATE", "DONE");
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
 
-        mPlayThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                byte[] writeData = new byte[mBufferSize];
-                FileInputStream fis = null;
+        mPlayThread = new Thread(() -> {
+            byte[] writeData = new byte[mBufferSize];
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(mFilepath);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            DataInputStream dis = new DataInputStream(fis);
+            mAudioTrack.play();
+
+            while (isPlaying) {
                 try {
-                    fis = new FileInputStream(mFilepath);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
+                    int ret = dis.read(writeData, 0, mBufferSize);
+                    if (ret <= 0) {
 
-                DataInputStream dis = new DataInputStream(fis);
-                mAudioTrack.play();
-
-                while(isPlaying) {
-                    try {
-                        int ret = dis.read(writeData, 0, mBufferSize);
-                        if (ret <= 0) {
-
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     }
-                }
-                mAudioTrack.stop();
-                mAudioTrack.release();
-                mAudioTrack = null;
-
-                try {
-                    dis.close();
-                    fis.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+            }
+            mAudioTrack.stop();
+            mAudioTrack.release();
+            mAudioTrack = null;
+
+            try {
+                dis.close();
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         });
     }
@@ -185,8 +204,8 @@ public class TrainingFragment extends Fragment {
     @Override
     public void onViewCreated (@NotNull View view,
                                Bundle savedInstanceState) {
-        Button bt_record = (Button)view.findViewById(R.id.bt_record);
-        Button bt_play = (Button)view.findViewById(R.id.bt_play);
+        Button bt_record = view.findViewById(R.id.bt_record);
+        Button bt_play = view.findViewById(R.id.bt_play);
         bt_record.setOnClickListener(this::onRecord);
         bt_play.setOnClickListener(this::onPlay);
     }
@@ -251,6 +270,37 @@ public class TrainingFragment extends Fragment {
                 'd', 'a', 't', 'a', // Chunk ID
                 0, 0, 0, 0, //Chunk Size (나중에 업데이트 될 것)
         });
+    }
+
+    public static void updateWavHeader(FileDescriptor fd, long length) throws IOException {
+        byte[] sizes1 = ByteBuffer
+                .allocate(4)
+                .order(ByteOrder.LITTLE_ENDIAN)
+                .putInt((int) (length - 8))
+                .array();
+        byte[] sizes2 = ByteBuffer
+                .allocate(4)
+                .order(ByteOrder.LITTLE_ENDIAN)
+                .putInt((int) (length - 44))
+                .array();
+        FileOutputStream fos = new FileOutputStream(fd);
+        try {
+            FileChannel ch = fos.getChannel();
+            ch.position(4);
+            ch.write(ByteBuffer.wrap(sizes1));
+            ch.position(40);
+            ch.write(ByteBuffer.wrap(sizes2));
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+
+                }
+            }
+        }
     }
 
     public String getRealPathFromURI(Context context, Uri contentUri) {
